aac604ceaa47b7b59109eb48dbd58a5be7c9ac9d
hdhdhdhd95 <a01229778@itesm.mx>
2016-03-14 17:55:40 -0600

    Signed-off-by: hdhdhdhd95 <a01229778@itesm.mx>

--- sim.py
+++ sim.py
@@ -3,38 +3,87 @@
 import time
 import pyupm_grove as grove
 import pyupm_ttp223 as ttp223
+import socket
+import fcntl
+import struct
+
+import pyupm_i2clcd as lcd
+
+
+def prepare_LCD():
+	# Initialize Jhd1313m1 at 0x3E (LCD_ADDRESS) and 0x62 (RGB_ADDRESS)
+	myLcd = lcd.Jhd1313m1(0, 0x3E, 0x62)
+	# Clear
+	myLcd.clear()
+	# Green
+	myLcd.setColor(255, 255, 0)
+	# Zero the cursor
+	myLcd.setCursor(0,0)
+	return myLcd
+	
+def print_capacities_LCD(zone_1_cap, zone_2_cap, myLcd):
+	myLcd.clear()
+	myLcd.setCursor(0,0)
+	myLcd.write("Zone 1: " + str(zone_1_cap) + " SPOTS.")
+	myLcd.setCursor(1,0)
+	myLcd.write("Zone 2: " + str(zone_2_cap) + " SPOTS.")
+
+def print_text_LCD(string, string2, myLcd):
+	myLcd.clear()
+	myLcd.setCursor(0,0)
+	myLcd.write(string)
+	myLcd.setCursor(1,0)
+	myLcd.write(string2)
+	
+
 
 def print_capacities(zone_1_cap, zone_2_cap):
-    print 'Zone 1 capacity: ' + str(zone_1_cap)
-    print 'Zone 2 capacity: ' + str(zone_2_cap)
+	print 'Zone 1 capacity: ' + str(zone_1_cap)
+	print 'Zone 2 capacity: ' + str(zone_2_cap)
 
 def listen(zone_1_cap, zone_2_cap):
-    touch = ttp223.TTP223(7)
-    button = grove.GroveButton(8)
-    while (1):
-        if button.value() == 1:
-            if zone_1_cap > 0:
-                zone_1_cap = zone_1_cap - 1
-            elif zone_2_cap > 0:
-                zone_2_cap = zone_2_cap - 1
-            elif zone_1_cap == 0 and zone_2_cap == 0:
-                print 'Parking full'
-            print_capacities(zone_1_cap, zone_2_cap)
-            time.sleep(0.2)
+	touch = ttp223.TTP223(3)
+	button = grove.GroveButton(4)
+	button2 = grove.GroveButton(8)
+	touch2 = ttp223.TTP223(7)
+	
+	myLcd = prepare_LCD()
+	print_capacities(zone_1_cap, zone_2_cap)
+	print_capacities_LCD(zone_1_cap, zone_2_cap, myLcd)
+	while (1):
+		if button.value() == 1:
+			if zone_1_cap > 0:
+				zone_1_cap = zone_1_cap - 1
+				print_capacities(zone_1_cap, zone_2_cap)
+				print_capacities_LCD(zone_1_cap, zone_2_cap, myLcd)
+		elif button2.value() == 1:
+			if zone_2_cap > 0 :
+				zone_2_cap = zone_2_cap - 1	
+				print_capacities(zone_1_cap, zone_2_cap)
+				print_capacities_LCD(zone_1_cap, zone_2_cap, myLcd)
+		elif zone_1_cap == 0 and zone_2_cap == 0:
+			print 'Parking full'
+			print_text_LCD("Parking Full :(", "Please wait.", myLcd)
+		time.sleep(1)
 
-        if touch.isPressed():
-            if zone_2_cap == 20:
-                zone_1_cap = zone_1_cap + 1
-            elif zone_1_cap == 0:
-                zone_2_cap = zone_2_cap + 1
-            print_capacities(zone_1_cap, zone_2_cap)
-            time.sleep(0.2)
+		if touch.isPressed():
+			if zone_1_cap < 20:
+				zone_1_cap = zone_1_cap + 1
+				print_capacities(zone_1_cap, zone_2_cap)
+				print_capacities_LCD(zone_1_cap, zone_2_cap, myLcd)
+		elif touch2.isPressed():
+			if zone_2_cap < 20:
+				zone_2_cap = zone_2_cap + 1
+				print_capacities(zone_1_cap, zone_2_cap)
+				print_capacities_LCD(zone_1_cap, zone_2_cap, myLcd)
+		time.sleep(.5)
 
 def main():
-    zone_1_cap = 20
-    zone_2_cap = 20
+	zone_1_cap = 20
+	zone_2_cap = 20
 
-    listen(zone_1_cap, zone_2_cap)
+	listen(zone_1_cap, zone_2_cap)
+	
 
 if __name__ == '__main__':
-    main()
+	main()
